86061762d9004eba7794ade61b6ffffa6e1b2830
joyboy18900 <surakiat.dev@gmail.com>
2025-09-29 01:49:25 +0700

    Update lession project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, Rust!");
+    let crabby_treasure = String::from("gold coins");
+    let borrow_treasure = &crabby_treasure;
+
+    println!("Borrowed treasure: {}", borrow_treasure);
+    println!("Original treasure: {}", crabby_treasure);
 }
